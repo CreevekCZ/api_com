a8c0c6b7727379789410e0343e67c9394f1bb9ef
jankoznarek <jan@koznarek.cz>
2021-10-01 16:32:34 +0200

    Initial commit

---
+++ lib/api_com.dart
@@ -0,0 +1,2 @@
+export 'src/models/base_response.dart' show BaseResponse;
+export 'src/models/base_request.dart' show BaseRequest;
---
+++ lib/src/enums/response_status.dart
@@ -0,0 +1,11 @@
+enum ResponseStatus {
+  success,
+  created,
+  accepted,
+  noContent,
+  serverError,
+  error,
+  unknownStatus,
+  notFound,
+  none,
+}
