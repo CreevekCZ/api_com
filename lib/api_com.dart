734c2c2811543af1748d4a741deaaa79cc20841c
jankoznarek <jan@koznarek.cz>
2022-09-19 18:16:45 +0200

    fix: Export DateTimeAtomExtension

--- lib/api_com.dart
+++ lib/api_com.dart
@@ -4,6 +4,8 @@ export 'src/enums/enums.dart';
 export 'src/models/models.dart';
 export 'src/core/com_config.dart';
 import 'src/core/com.dart';
+export 'src/extensions/date_time_atom_extension.dart'
+    show DateTimeAtomExtension;
 export 'src/extensions/uri_php_extension.dart' show UriPhpEstension;
 
 class _ComImpl extends ComInterface {}
